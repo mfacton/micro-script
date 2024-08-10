528f2a65ef78bcd111cc66afc0653d3eb6d5494b
Miguel Flores-Acton <mfacton1@gmail.com>
2024-08-09 20:08:57 -0400

    Save

---
+++ serial_print.py
@@ -0,0 +1,11 @@
+import time
+
+from tools.serial_manager import SerialManager
+
+manager = SerialManager("slc-control", baud=38400)
+
+while True:
+    data = manager.read_all()
+    if data:
+        print(data.decode())
+    time.sleep(0.01)
