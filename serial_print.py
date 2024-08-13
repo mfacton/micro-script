dc72159c519b80363e696779e778ee865b042815
Miguel Flores-Acton <mfacton1@gmail.com>
2024-08-13 10:30:32 -0400

    Save

--- serial_print.py
+++ serial_print.py
@@ -2,10 +2,10 @@ import time
 
 from tools.serial_manager import SerialManager
 
-manager = SerialManager("slc-control", baud=38400)
+manager = SerialManager("slc-control-lib", baud=38400)
 
 while True:
     data = manager.read_all()
     if data:
         print(data.decode())
-    time.sleep(0.01)
+    time.sleep(0.001)
